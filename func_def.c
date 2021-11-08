cbe2797a03e036cb54a81003e7a96b50479c60de
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-08 20:01:37 +0000

    pushing _writenum, func_def.c, main.h. Betty checked

---
+++ func_def.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+  * format_char - Prints a char.
+  * @c: list of arguments.
+  * Return: The amount of chars entered.
+  */
+
+int format_char(va_list c)
+{
+	_write(va_arg(c, int));
+	return (1);
+}
+
+/**
+  * format_string - Prints a string
+  * @s: list of arguments
+  * Return: The string.
+  */
+
+int format_string(va_list s)
+{
+	int i = 0;
+	char *sm;
+
+	sm = va_arg(s, char*);
+
+	if (sm == NULL)
+	sm = "(null)";
+	for (; sm[i] != '\0'; i++)
+	{
+		_write(sm[i]);
+	}
+	return (i);
+}
+
+/**
+  * format_percent - Prints a porcent
+  * @percent: List of arguments
+  * Return: the string
+  */
+
+int format_percent(va_list percent)
+{
+	(void)percent;
+
+	_write('%');
+	return (1);
+}
+
+/**
+  * format_decimal - prints decimal and ints
+  * @num: number provided
+  * Return: amount of chars added
+  */
+int format_decimal(va_list num)
+{
+	int i = 0;
+	int final_print = 0;
+
+	i = va_arg(num, int);
+	final_print = _writenum(i);
+	return (final print);
+}
--- main.h
+++ main.h
@@ -3,12 +3,25 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+  * struct my_printf  - struct for the _printf function
+  * @name: the function
+  * @f: function associated
+  */
+
 typedef struct my_printf
 {
-    char *name;
-    void (*f)(va_list);
-}print_t;
+	char *name;
+	int (*f)(va_list);
+} print_s;
 
 int _printf(const char *format, ...);
+int _write(char c);
+int _wrintenum(int num);
+int get_funk(const char *format, print_s spec[], va_list info);
+int format_char(va_list c);
+int format_string(va_list s);
+int format_percent(va_list percent);
+int format_decimal(va_list num);
 
 #endif
