c2ed3512b33e092d6211c11bb35563cbcc4ca2ee
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-09 16:46:35 +0000

    several test done for task 1

--- main.c
+++ main.c
@@ -8,10 +8,22 @@
   */
 int main(void)
 {
-	_printf("%d\n", INT_MAX);
-	printf("%d\n", INT_MAX);
+	int len = 0, len2 = 0;
 
-    _printf("%d\n", INT_MIN);
-    printf("%d\n", INT_MIN);
+	_printf("Case 1: int 0 [%d]\n", (int)0);
+	printf("Case 1: int 0 [%d]\n", (int)0);
+
+	_printf("Case 2: operations between min and max [%i + %i = %i]\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+	printf("Case 2: operations between min and max [%i + %i = %i]\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+	
+/**	len = _printf("%d");
+	printf("\n");
+	printf("entre los dos\n");
+	len2 = printf("%d");
+	printf("\n"); */
+
+	_printf("%d\n", len);
+	printf("%d\n", len2);
+
 	return (0);
 }
